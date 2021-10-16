78c5c60b165de11f00e120a6cd357b29c6ec70c9
Tomáš Raček <tom@krab1k.net>
2021-10-16 14:32:34 +0200

    [utils/stress_search] Add random sleep and fix reading schema file

--- utils/stress_search.py
+++ utils/stress_search.py
@@ -1,12 +1,13 @@
 import argparse
 import json
+import os.path
 from concurrent.futures import as_completed, ThreadPoolExecutor
-import functools
 import sys
 import random
 import mariadb
 import string
 import tqdm
+import time
 import requests
 from jsonschema import validate, ValidationError
 from typing import List
@@ -28,7 +29,9 @@ def get_ids(min_size: int, max_size: int, limit: int) -> List[str]:
     return data
 
 
-def run_search(pdb_id: str, num_results: int, radius: float, schema: str):
+def run_search(pdb_id: str, num_results: int, radius: float, schema: str, sleep: bool):
+    if sleep:
+        time.sleep(random.random())
     url = f'http://similar-pdb.cerit-sc.cz:{PORTS["full"]}/search'
     job_id = ''.join(random.choice(string.ascii_lowercase) for _ in range(8))
     parameters = {'queryid': pdb_id, 'k': num_results, 'job_id': job_id, 'radius': radius}
@@ -44,14 +47,14 @@ def run_search(pdb_id: str, num_results: int, radius: float, schema: str):
     return pdb_id, 'OK'
 
 
-def stress(ids: List[str], num_results: int, radius: float, workers: int):
-    with open('response_schema.json') as f:
+def stress(ids: List[str], num_results: int, radius: float, workers: int, sleep: bool):
+    with open(os.path.join(os.path.dirname(__file__), 'response_schema.json')) as f:
         schema = json.load(f)
 
     executor = ThreadPoolExecutor(workers)
 
-    run_search2 = functools.partial(run_search, num_results=num_results, radius=radius, schema=schema)
-    jobs = [executor.submit(run_search2, data) for data in ids]
+    jobs = [executor.submit(run_search, data, num_results=num_results, radius=radius, schema=schema, sleep=sleep)
+            for data in ids]
     results = []
     for job in tqdm.tqdm(as_completed(jobs), total=len(jobs)):
         results.append((job.result()))
@@ -79,9 +82,11 @@ if __name__ == '__main__':
     parser.add_argument('--radius', type=float, default=0.5, help='Radius for search')
     parser.add_argument('--results', type=int, default=30, help='Number of results for search')
     parser.add_argument('--workers', type=int, default=1, help='Number of concurrent requests')
+    parser.add_argument('--sleep', dest='sleep', action='store_true', default=False,
+                        help='Random sleep before request')
 
     args = parser.parse_args()
 
     pdb_ids = get_ids(args.min_size, args.max_size, args.count)
 
-    stress(pdb_ids, num_results=args.results, radius=args.radius, workers=args.workers)
+    stress(pdb_ids, num_results=args.results, radius=args.radius, workers=args.workers, sleep=args.sleep)
